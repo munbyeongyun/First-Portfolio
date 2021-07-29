edf248065e355408fe70e629ee50bd1985008abd
munbyeongyun <dkqk1326@naver.com>
2021-07-29 16:44:38 +0900

    유튜브 사이트 만들기

---
+++ boilerplate-mern-stack-master/server/models/Video.js
@@ -0,0 +1,43 @@
+const { Schema } = require("mongoose");
+const Schema = mongoose.Schema;
+const mongoose = require("mongoose");
+
+const videouserSchema = mongoose.Schema(
+  {
+    writer: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    title: {
+      type: String,
+      maxlength: 50,
+    },
+    description: {
+      type: String,
+    },
+    privacy: {
+      type: Number,
+    },
+    filePath: {
+      type: String,
+    },
+    category: {
+      type: String,
+    },
+    views: {
+      type: Number,
+      default: 0,
+    },
+    duration: {
+      type: String,
+    },
+    thumbnail: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+const Video = mongoose.model("Video", VideoSchema);
+
+module.exports = { Video };
